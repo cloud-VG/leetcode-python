010324eef7bdcb01dc7802dc240989b69027ae19
vishwajeet <cloudmail.vishwajeet@gmail.com>
2021-01-19 14:59:19 +0530

    P:8

---
+++ first_unique_char.py
@@ -0,0 +1,15 @@
+__author__ = 'Vishwajeet Ghatage'
+__email__ = 'cloudmail.vishwajeet@gmail.com'
+__date__ = '19/01/2021'
+
+
+def solution(s: str) -> int:
+    for i, char in enumerate(s):
+        if s.count(char) == 1:
+            return i
+    return -1
+
+
+if __name__ == '__main__':
+    S = "loveleetcode"
+    print(solution(S))
