231d24d54076c50d09d3c3b49791c9de94dffbed
vishwajeet <cloudmail.vishwajeet@gmail.com>
2021-01-22 00:10:26 +0530

    P:12

--- backspace_string.py
+++ backspace_string.py
@@ -3,20 +3,18 @@ __email__ = 'cloudmail.vishwajeet@gmail.com'
 __date__ = '21/01/2021'
 
 
-def solution(s: str, t: str) -> bool:
+def helper(s: str) -> str:
     list_s = []
-    list_t = []
     for c in s:
         if c != '#':
             list_s.append(c)
         elif list_s:
             list_s.pop()
-    for c in t:
-        if c != '#':
-            list_t.append(c)
-        elif list_t:
-            list_t.pop()
-    return ''.join(list_s) == ''.join(list_t)
+    return ''.join(list_s)
+
+
+def solution(s: str, t: str) -> bool:
+    return helper(s) == helper(t)
 
 
 if __name__ == '__main__':
