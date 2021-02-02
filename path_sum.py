16d4939d74d0bf1bff099e9658d3645bf2229df9
vishwajeet <cloudmail.vishwajeet@gmail.com>
2021-02-02 21:44:03 +0530

    P:27

---
+++ path_sum.py
@@ -0,0 +1,21 @@
+__author__ = 'Vishwajeet Ghatage'
+__email__ = 'cloudmail.vishwajeet@gmail.com'
+__date__ = '02/02/2021'
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        elif not root.left and not root.right and targetSum - root.val == 0:
+            return True
+        else:
+            return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
